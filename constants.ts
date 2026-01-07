15b3cdbf978a27e2b1305890ec2757bf78344b9e
DjukicAleksej <aleksejdjukicets@gmail.com>
2026-01-07 18:34:46 +0100

    The positions for the 4 lanes in which the orbs the user will be hittimng are done , in the x position it goes from left to right in y from low to high

--- constants.ts
+++ constants.ts
@@ -11,3 +11,9 @@ export const NOTE_SPEED = 10; //Reduced from 15 for easier difficulty
 export const LANE_WIDTH = 0.8;
 export const LAYER_HEIGHT = 0.8;
 export const NOTE_SIZE = 0.5;
+
+
+//pos for the 4 lanes (centered arnd 0)
+export const LANE_X_POSITIONS = [-1.5 * LANE_WIDTH, -0.5 * LANE_WIDTH,0.5 * LANE_WIDTH, 1.5 * LANE_WIDTH];
+export const LAYER_Y_POSITIONS = [0.8, 1.6,2.4]; // low , mid and highest
+
