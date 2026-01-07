451a6b78048d0578f899843e826f8f322d86b6c0
DjukicAleksej <aleksejdjukicets@gmail.com>
2026-01-07 11:49:12 +0100

    I added a hand type because I will track both hands of you and then we will have different kinds of movement like up down left right and any of them

--- types.ts
+++ types.ts
@@ -8,5 +8,15 @@ export enum GameStatus {
     VICTORY='VICTORY'
 }
 
+export type HandType = 'left' | 'right';
+//0 for up , 1 down , 2 is left , 3 is right and 4 any
+
+export enum CutDirection {
+    UP = 0,
+    DOWN = 1,
+    LEFT = 2,
+    RIGHT = 3,
+    ANY = 4
+}
 
 
