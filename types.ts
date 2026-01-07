98ffc7f277120f5c64126842a18b76931ec53b3b
DjukicAleksej <aleksejdjukicets@gmail.com>
2026-01-07 11:41:32 +0100

    Added game status to the types , so we can upodate the status if we are plating idle loading etc and have different looks of website depending on the status

---
+++ types.ts
@@ -0,0 +1,12 @@
+import * as THREE from 'three';
+
+export enum GameStatus {
+    LOADING = 'LOADING',
+    IDLE = 'IDLE',
+    PLAYING='PLAYING',
+    GAME_OVER = 'GAME_OVER',
+    VICTORY='VICTORY'
+}
+
+
+
