603b97747e8469623d68f68c71f1155935b76e5f
DjukicAleksej <aleksejdjukicets@gmail.com>
2026-01-07 18:29:28 +0100

    I added initial game world configs for the player

---
+++ constants.ts
@@ -0,0 +1,13 @@
+import { CutDirection, NoteData } from "./types";
+import * as THREE from 'three';
+
+//Game world config
+export const TRACK_LENGTH = 50;
+export const SPAWN_Z = -30;
+export const PLAYER_Z = 0;
+export const MISS_z = 5;
+export const NOTE_SPEED = 10; //Reduced from 15 for easier difficulty
+
+export const LANE_WIDTH = 0.8;
+export const LAYER_HEIGHT = 0.8;
+export const NOTE_SIZE = 0.5;
