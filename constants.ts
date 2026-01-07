4aaeb189989a6549e2c6d2056ebb1c8cc0775d6e
DjukicAleksej <aleksejdjukicets@gmail.com>
2026-01-07 18:57:53 +0100

    Added audio logic of how it will work, so basically we will use the song url from a few songs I upload , I thought of users putting YT link of the song they want but I cant get the audio file from it because of CORS so I will upload a few good song choices

--- constants.ts
+++ constants.ts
@@ -17,3 +17,8 @@ export const NOTE_SIZE = 0.5;
 export const LANE_X_POSITIONS = [-1.5 * LANE_WIDTH, -0.5 * LANE_WIDTH,0.5 * LANE_WIDTH, 1.5 * LANE_WIDTH];
 export const LAYER_Y_POSITIONS = [0.8, 1.6,2.4]; // low , mid and highest
 
+//Audio
+//Using a solid rhytmic track that is free to use
+export const SONG_URL = '';
+export const SONG_BPM = 140;
+const BEAT_TIME = 60/ SONG_BPM;
